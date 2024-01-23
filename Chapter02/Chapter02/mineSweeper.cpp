cc3a0e8b6dea21773e307f688c8ab129d1d64646
hmh4947 <hmh4947@naver.com>
2024-01-23 15:14:02 +0900

    Chapter02.10

---
+++ Chapter02/Chapter02/mineSweeper.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main() {
+	int x, y,k;
+	
+	char board[10][10] = {0};
+	srand(time(NULL));
+
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (rand() % 100 < 30) {
+				board[i][j] = true;
+			}
+		}
+	}
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (board[i][j]) {
+				cout << "# ";
+			}
+			else {
+				cout << ". ";
+			}
+		}
+		cout << endl;
+	}
+
+	return 0;
+
+}
