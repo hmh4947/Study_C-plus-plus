647c63dd58affb65411c94ebbbfb8ec39bb2a0d1
hmh4947 <hmh4947@naver.com>
2024-03-04 17:55:33 +0900

    Chapter03

---
+++ Chapter03/Chapter03/lab_overloaded_func.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<stdlib.h>	//abs함수(int형)
+#include<cmath>	//fabs함수(double형)
+using namespace std;
+
+
+void print(int i) {
+	cout << "정수 출력: " << i << endl;
+
+}
+void print(double i) {
+	cout << "실수 출력: " << i << endl;
+}
+void print(char i) {
+	cout << "문자 출력: " << i << endl;
+}
+int absolute(int i) {
+	int result;
+	result = abs(i);
+	//result = (i > 0) ? i : -i;
+	cout << "절대값 정수 출력: " << result << endl;
+	return result;
+}
+double absolute(double i) {
+	double result;
+	result = fabs(i);
+	//result = (i > 0.) ? i : -i;
+	cout << "절대값 실수 출력: " << result << endl;
+	return result;
+}
+int main2() {
+	print(100);
+	print(3.14);
+	print('C');
+	absolute(-2);
+	absolute(-1.0);
+	return 0;
+}
---
+++ Chapter03/Chapter03/overloaded_func.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+//정수값을 제곱하는 함수
+int square(int i) {
+
+	cout << "square(int) 호출" << endl;
+	return i * i;
+}
+//실수값을 제곱하는 함수
+int square(double i) {
+
+	cout << "square(double) 호출" << endl;
+	return i * i;
+}
+int main1() {
+	cout << square(10) << endl;
+	cout << square(2.0) << endl;
+	return 0;
+}
