90f82e9ab5182735e7e63e339d4e6e282723a35f
hmh4947 <hmh4947@naver.com>
2024-03-05 15:16:59 +0900

    ProgrammingExercise03

---
+++ ProgrammingExercise03/ProgrammingExercise03/ProgrammingExercise4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+/*int prime() {
+	for (int i = 2; i < 100; i++) {
+		if ((i - 1) % 2 != 0) {
+			return i;
+		}
+		
+	}
+}
+int main4() {
+	for(int i=2;i<100;i++){
+		cout << prime();
+	}
+	return 0;
+}
---
+++ ProgrammingExercise03/ProgrammingExercise03/ProgrammingExercise5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+/*string sayHello(int n = 1) {
+	for (int i = 0; i < n; i++) {
+		
+	}
+		
+}
+int main() {
+	int num;
+	cin >> num;
+	cout<<sayHello(num);
+	return 0;
+}
