6670435981956a024728f46559f89aa380bf4d22
hmh4947 <73475672+hmh4947@users.noreply.github.com>
2024-03-10 12:09:53 +0900

    Delete Chapter04 directory

--- Chapter04/Chapter04/car.cpp
+++
@@ -1,8 +0,0 @@
-#include"car.h"
-int Car::getSpeed() {
-	return speed;
-}
-void Car::setSpeed(int s) {
-	speed = s;
-
-}
--- Chapter04/Chapter04/main.cpp
+++
@@ -1,8 +0,0 @@
-#include"car.h"
-using namespace std;
-int main7() {
-	Car myCar;
-	myCar.setSpeed(80);
-	cout << "현재 속도는 " << myCar.getSpeed() << endl;
-	return 0;
-}
