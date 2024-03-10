6f5b8af61bd42a680d09b3fb2b0b3c5d6f980525
hmh4947 <hmh4947@naver.com>
2024-03-10 12:24:53 +0900

    Revert "Delete Chapter01 directory"
    This reverts commit 5aeb67cd4dd82235be8d92c1c3f127758794f8ee.

---
+++ Chapter01/Chapter01/cin.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main3() {
+	string name;
+	cout << "이름을 입력하시오: ";
+	cin >> name;
+	cout << name << "을 환영합니다." << endl;
+	return 0;
+}
