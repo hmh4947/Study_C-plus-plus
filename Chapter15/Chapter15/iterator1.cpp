8a051a0b08ca1ab017f5e75c472759d7cc6e24a8
hmh4947 <hmh4947@naver.com>
2024-04-10 18:48:18 +0900

    Chapter15

---
+++ Chapter15/Chapter15/iterator1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main2() {
+	vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+
+	for (vector<int>::iterator p = v.begin(); p != v.end(); ++p) {
+		cout << *p << endl;
+		
+	}
+	return 0;
+}
---
+++ Chapter15/Chapter15/vector.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include<time.h>
+#include<list>
+using namespace std;
+
+int main1()
+{
+	list<int> values;
+
+	srand(time(NULL));
+	for (int i = 0; i < 10; i++) {
+		values.push_back(rand() % 100);
+
+		
+	}
+	values.sort();
+
+	for (auto& e : values) {
+		std::cout << e << ' ';
+	}
+	std::cout << endl;
+	return 0;
+}
